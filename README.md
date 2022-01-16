1bd40a3388c4b80100a42f9c294599c8845c6f0d
programwisachin <sachinacharya125@gmail.com>
2022-01-16 20:28:57 +0530

    Updating Banner with slider effect

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # Show Time Website
 This website fetches popular movies and TV shows for you.
-
+![Preview](/showtime_preview.png)
 
 ## Components used in this project.
 
 
 ### Banner
-This component displays banner .
+This component displays banner. A slider with arrow to navigate forward and backward.Click on watch now button to watch the trailer.
 
 ### Poster
-This component displays movie and show poster cards.
+This component displays movie and show poster cards.Click on poster to watch movie/show trailer, to close the trailer again click on any poster card.
 
 ## Overview 
 In this project website is build using REACT JS. 
-Fetches movies and shows from API and displays its card effectively to the user
+Fetches movies and shows from API and displays its card effectively to the user with trailer.
 
 ## API
 API link to fetch movies and shows is https://www.themoviedb.org/
--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,86 +1,131 @@
 import React, { useState, useEffect } from "react";
 import "./Banner.css";
-import YouTube from 'react-youtube';
-import movieTrailer from 'movie-trailer'
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+import NavigateNextOutlinedIcon from "@mui/icons-material/NavigateNextOutlined";
+import NavigateBeforeOutlinedIcon from "@mui/icons-material/NavigateBeforeOutlined";
+
 function Banner({ fetchUrl }) {
 	const baseUrl = "https://api.themoviedb.org/3"; //base url required to make any request
-	const [movie, setMovie] = useState([]);
-	const [trailerUrl, setTrailerUrl] = useState("") //storing url of the trailer
-	const [buttonState, setButtonState] = useState(true)
-
+	const [movies, setMovies] = useState([]);
+	const [trailerUrl, setTrailerUrl] = useState(""); //storing url of the trailer
+	const [buttonState, setButtonState] = useState(true);
+	const [currentPage, setCurrentPage] = useState(0);
+	const length = movies.length;
 	useEffect(() => {
 		async function fetchData() {
 			const response = await fetch(`${baseUrl}${fetchUrl}`);
 			const json = await response.json();
-			setMovie(
-				json.results[
-					Math.floor(Math.random() * json.results.length - 1)
-				]
-			); //fetching random movie from the response
+			setMovies(json.results.slice(0, 4)); //fetching random movie from the response
+			console.log(json.results.slice(0, 4));
 			return response;
 		}
 		fetchData();
 		// eslint-disable-next-line
 	}, []);
-	const opts = { //options for displaying trailer
+	const opts = {
+		//options for displaying trailer
 
-        height: '390',
-        width: '890',
-        playerVars: {
-          // https://developers.google.com/youtube/player_parameters
-          autoplay: 1,
-        },
-    }
+		height: "390",
+		width: "890",
+		playerVars: {
+			// https://developers.google.com/youtube/player_parameters
+			autoplay: 1,
+		},
+	};
 
-    const handleClick = (movie)=>{
+	const handleClick = (movie) => {
+		if (trailerUrl) {
+			setButtonState(true);
+			setTrailerUrl("");
+		} else {
+			movieTrailer(
+				movie?.original_name || movie?.name || movie?.title || ""
+			)
+				.then((url) => {
+					const urlParam = new URLSearchParams(new URL(url).search); //grabbing essential id (v=...) from the url
+					setTrailerUrl(urlParam.get("v"));
+					setButtonState(false);
+				})
+				.catch((error) => {
+					console.log(error.message); // if error occured display it on console
+				});
+		}
+	};
 
-        if(trailerUrl){
-			setButtonState(true)
-            setTrailerUrl("")
-        }
-        else{
-            movieTrailer(movie?.original_name || movie?.name || movie?.title || "")
-            .then((url)=>{
-                const urlParam = new URLSearchParams(new URL(url).search)  //grabbing essential id (v=...) from the url
-                setTrailerUrl(urlParam.get('v'))
-				setButtonState(false)
-            }).catch((error)=>{
-                console.log(error.message)  // if error occured display it on console
-            })
-        }
-    }
-      
+	const nextSlide = () => {
+		setCurrentPage(currentPage === length - 1 ? 0 : currentPage + 1);
+	};
+
+	const prevSlide = () => {
+		setCurrentPage(currentPage === 0 ? length - 1 : currentPage - 1);
+	};
+
 	return (
 		<header>
-			<div className="banner" style={{display:"flex",justifyContent:"space-between"}}>
-			<div className="bannerContent">
-				<h1 className="bannerHead">
-					{movie?.name || movie?.original_name || movie?.title}
-				</h1>
-				{/*setting title*/}
-				<h1 className="bannerDesc">
-					{movie?.overview ? movie?.overview.slice(0, 150) : ""}...
-					{/*setting description of banner of only 150 characters*/}
-				</h1>
-				<button className="bannerBtn"  onClick={()=>{handleClick(movie)}}>{buttonState?"Watch Now":"Close"}</button> {/*button to play banner show/movie */}
-			</div>
-			{/*setting background image of the banner*/}
+			<NavigateBeforeOutlinedIcon
+				style={{ fontSize: "3em", color: "white" }}
+				className="leftArrow"
+				onClick={prevSlide}
+			/>
+			<NavigateNextOutlinedIcon
+				style={{ fontSize: "3em", color: "white" }}
+				className="rightArrow"
+				onClick={nextSlide}
+			/>
+			{movies.map((movie, index) => {
+				return (
+					<>
+						{index === currentPage && (
+							<div className="banner" key={index}>
+								<div className="bannerContent">
+									<h1 className="bannerHead">
+										{movie?.name ||
+											movie?.original_name ||
+											movie?.title}
+									</h1>
+									<h1 className="bannerDesc">
+										{movie?.overview
+											? movie?.overview.slice(0, 150)
+											: ""}
+										...
+									</h1>
+									<button
+										className="bannerBtn"
+										onClick={() => {
+											handleClick(movie);
+										}}
+									>
+										{buttonState ? "Watch Now " : "Close"}
+									</button>
+								</div>
+								<div
+									className="bannerImg"
+									style={{
+										backgroundSize: "cover",
+										backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+										backgroundPosition: "center center",
+										width: "65%",
+									}}
+								>
+									<div className="bannerFade"></div>
+								</div>
+							</div>
+						)}
+					</>
+				);
+			})}
+
 			<div
-				className="bannerImg"
 				style={{
-					backgroundSize: "cover",
-					backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
-					backgroundPosition: "center center",
-					width: "65%",
+					display: "flex",
+					justifyContent: "center",
+					padding: "8px",
 				}}
-				>
-				{/*div for fading effect on banner image*/}
-				<div className="bannerFade"></div>
+			>
+				{trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}{" "}
+				{/*trailer block*/}
 			</div>
-			</div>
-			<div style={{display:"flex",justifyContent:"center",padding:"8px"}}>
-                {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}  {/*trailer block*/}
-            </div>
 		</header>
 	);
 }
